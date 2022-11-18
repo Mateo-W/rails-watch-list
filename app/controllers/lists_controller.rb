0ec0c42ac2379f259429eef957d599ec1aca139e
Mateo Westphal <mateojwestphal@gmail.com>
2022-11-18 12:11:54 +0000

    copied code

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,13 @@
 class ListsController < ApplicationController
-  before_action :set_restaurant, only: [:show]
+  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
   end
 
   def new
@@ -14,13 +15,26 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.create(list_params)
-
-    redirect_to list_path(@list)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-end
 
-private
-def list_params
-  params.request(:list).permit(:name)
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
 end
